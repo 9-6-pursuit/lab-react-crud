8e9441948dcfd68ec0692aab666690ba96640475
Lakisha J <lakishajohnson@pursuit.org>
2023-05-17 15:54:51 -0400

    Set variable for process.env. and updated four functions

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,3 +1,4 @@
+const URL = process.env.REACT_APP_API_BASE_URL;
 // Shows
 
 // Create
@@ -7,17 +8,20 @@ export function createShow(show) {
 
 // Delete
 export function destroyShow(id) {
-  return;
+  const options = { method: "DELETE" };
+  return fetch(`${URL}/shows/${id}`, options);
 }
 
 // Index/Get all
-export function getAllShows() {
-  return;
+export async function getAllShows() {
+  const response = await fetch(`${URL}/shows`);
+  return await response.json();
 }
 
 // Show/Get one
-export function getOneShow(id) {
-  return;
+export async function getOneShow(id) {
+  const response = await fetch(`${URL}/shows/${id}`);
+  return await response.json();
 }
 
 // Update
@@ -27,6 +31,7 @@ export function updateShow(id, show) {
 
 // Movies
 
-export function getAllMovies() {
-  return;
+export async function getAllMovies() {
+  const response = await fetch(`${URL}/movies`);
+  return await response.json();
 }
