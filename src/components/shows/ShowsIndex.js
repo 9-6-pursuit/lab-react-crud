a39c3990468516a2a990fecd0f247db51d654af0
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-05-21 08:09:17 -0400

    For now

--- src/components/shows/ShowsIndex.js
+++ src/components/shows/ShowsIndex.js
@@ -1,13 +1,46 @@
 import { Link } from "react-router-dom";
 
+import { useEffect, useState } from "react";
+
+import { getAllShows } from "../../api/fetch"
+
 import ErrorMessage from "../errors/ErrorMessage";
+
+import ShowListing from "./ShowListing"
 
 import "./ShowsIndex.css";
 
 export default function ShowsIndex() {
+  const [loadingError, setLoadingError] = useState(false)
+  const [shows, setShows] = useState([])
+  const [allShows, setAllShows] = useState([])
+  const [searchTitle, setSearchTitle] = useState("")
+
+  function handleTextChange(event) {
+    const title = event.target.value;
+    const result = title.length ? filterShows(title, allShows) : allShows;
+    setSearchTitle(title)
+    setShows(result)
+  }
+
+
+  useEffect(() => {
+    
+    getAllShows().then((response)=>{
+      setShows(response)
+    }).catch((error)=> {
+        console.error(error)
+    })
+    .catch((error)=>{
+      console.error(error)
+      cons
+    })
+  }, [])
+
+
   return (
     <div>
-      {false ? (
+      {loadingError ? (
         <ErrorMessage />
       ) : (
         <section className="shows-index-wrapper">
@@ -27,6 +60,9 @@ export default function ShowsIndex() {
           </label>
           <section className="shows-index">
             {/* <!-- ShowListing components --> */}
+            {shows.map((show) =>{
+              return <ShowListing key = {show.id} show = {show} />
+            })}
           </section>
         </section>
       )}
