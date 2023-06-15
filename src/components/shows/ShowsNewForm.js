4aa18bf6c5b0bd9422c9f3e7bb75a184789dcbe2
RadiantRay22 <rayrussell@pursuit.org>
2023-06-15 06:43:03 -0400

    partial working

--- src/components/shows/ShowsNewForm.js
+++ src/components/shows/ShowsNewForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { createShow } from "../../api/fetch";
+import { useNavigate } from "react-router-dom";
 import "./ShowsForm.css";
 
 export default function ShowsForm() {
@@ -15,7 +16,18 @@ export default function ShowsForm() {
     releaseYear: "",
   });
 
-  function handleSubmit(event) {}
+  let navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    createShow(show)
+      .then((response) => {
+        navigate(`/shows/${response.id}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   function handleTextChange(event) {
     setShow({
